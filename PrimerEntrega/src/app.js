f951e33fffa8e4d456f8d00a4d70296f23a2536f
HuunterSG <agustinjperez.r@gmail.com>
2023-04-12 15:43:38 -0300

    agregamos cart

--- PrimerEntrega/src/app.js
+++ PrimerEntrega/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import productRouter from './routes/routesProduct.js'
+import cartRouter from './routes/routesCart.js'
 
 const app = express()
 const PORT = 8080
@@ -11,5 +12,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use('/api/products', productRouter)
+app.use('/api/carts', cartRouter)
 
 server.on('error',(error)=>console.log(`error del servidor: ${error}`))
